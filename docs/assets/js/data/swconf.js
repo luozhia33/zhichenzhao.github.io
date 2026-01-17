7298def0c9fcaf2a462c67fc9ee47e32da145cd4
luozhia <zhichen@luozhiadeMacBook-Air.local>
2026-01-17 13:17:34 -0800

    change

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1768684318',resources: [
+    cacheName: 'chirpy-1768684620',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
